9f47b8302ee60278fc8d4240cbf0b245780b8670
Adetuyi Tolu Emmanuel <tolu.adetuyi@gmail.com>
2018-03-11 16:03:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,27 @@
 </head>
 
 <body>
-
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '749793591893239',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.12'
+    });
+  };
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+<div class="fb-customerchat"
+  page_id="865721926939715"
+>
+</div>
 <nav class="navbar navbar-transparent navbar-fixed-top" role="navigation">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
