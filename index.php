fa7020fc5cc6ef1d05648b2d4e706a6133b36ee6
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2020-07-11 04:07:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -66,11 +66,6 @@
                 </div>
                 <div class="level-right">
                     <!-- your list menu here -->
-                    <div class="navbar-menu align-items-right" >
-                        <a href="" data-aos="fade-left" data-aos-delay="400" class="btn btn-secondary mr-1 aos-init aos-animate">
-                            Become a Thread Agent
-                        </a>
-                    </div>
                     <div class="mobile-overlay">
                         <div class="mobile-overlay-bg"></div>
                         <div class="mobile-menu">
